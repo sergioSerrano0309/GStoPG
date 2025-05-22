4a1c998163c0cdfb68b3ea0dd2453d19164ec797
sergioSerrano0309 <sergioserranorodriguez9@gmail.com>
2025-05-22 15:10:35 +0200

    arreglando cositas

--- GStoPG.py
+++ GStoPG.py
@@ -71,4 +71,6 @@ def actualizar_empleados():
 
 # Ejecutar la app
 if __name__ == "__main__":
-    app.run(port=10000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
